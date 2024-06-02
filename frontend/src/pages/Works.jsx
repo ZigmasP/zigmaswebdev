68f8537106493216b8ee420ff1e09dd6f8bcbe23
ZigmasP <zigmas.1@gmail.com>
2024-06-02 17:52:49 +0300

    ideigtos papildomos bibliotekos

---
+++ frontend/src/pages/Works.jsx
@@ -0,0 +1,12 @@
+import { Link} from "react-router-dom";
+import "./Works.scss";
+
+const Works = () => {
+  return (
+    <div className="works-container">
+      <Link to="/" className="back-link">Į pagrindinį</Link>
+    </div>
+  );
+};
+
+export default Works;
