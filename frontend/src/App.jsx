39ec86f3fdb570de0a60dfb33c0158e66e75da90
ZigmasP <zigmas.1@gmail.com>
2024-06-05 11:55:39 +0300

    pakoreguota contacts ir app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Works from "./pages/Works";
-const App = () => {
+import Contacts from "./pages/Contacts";
 
+const App = () => {
+  
 return (
   <>
    <Header />
@@ -12,6 +14,7 @@ return (
       <Route path="/" element={<Home />} />;
       <Route path="/apie-mane" element={<About />} />;
       <Route path="/mano-darbai" element={<Works />} />;
+      <Route path="/kontaktai" element={<Contacts />} />
     </Routes>
   </>
  );
---
+++ frontend/src/pages/Contacts.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { FaBuilding, FaUniversity, FaMapMarkerAlt, FaEnvelope, FaPhone } from "react-icons/fa";
+import "./Contacts.scss";
+
+const Contacts = () => {
+  return (
+    <div className="contacts-container">
+      <Link to="/" className="back-link">Į pradinį</Link>
+      <section className="hero">
+        <h1>Kontaktai</h1>
+      </section>
+      <section className="contact-info">
+        <div className="contact-item">
+          <FaBuilding className="icon" />
+          <div>
+            <span>Įmonės pavadinimas: Mano įmonė</span>
+            <span>Įmonės kodas: 123456789</span>
+          </div> 
+        </div>
+        <div className="contact-item">
+          <FaMapMarkerAlt className="icon" />
+          <div>
+            <span>Adresas: Kruojos g. 8-56, Pakruojis, Lietuva</span>
+          </div>
+        </div>
+        <div className="contact-item">
+          <FaUniversity className="icon" />
+          <div>
+            <span>Banko sąskaitos nr.: LT737300010097043750, Swedbank</span>
+          </div>
+        </div>
+        <div className="contact-item">
+          <FaEnvelope className="icon" />
+          <div>
+            <span>El. paštas: zigmas.1@gmail.com</span>
+          </div>
+        </div>
+        <div className="contact-item">
+          <FaPhone className="icon" />
+          <div>
+            <span>Tel. nr.: +37060627573</span>
+          </div>
+        </div>
+      </section>   
+    </div>
+  );
+};
+
+export default Contacts;
